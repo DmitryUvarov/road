2b404f8e7821ebbb0678b5cf33751ae85c520b6f
Dima <uvarov3123@gmail.com>
2023-11-01 20:00:25 +0200

    add blog pages

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,16 +3,48 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-// window.addEventListener('load', pageLoad)
+window.addEventListener('load', pageLoaded)
 
-// function pageLoad() {
-//     const htmlTag = document.documentElement
+function pageLoaded() {
 
-//     document.addEventListener('click', e => {
-//         const targetElement = e.target
+    document.addEventListener('click', (e) => {
+        const targetElement = e.target;
+    })
 
-//         if (targetElement.closest('.class')) {
-//             console.log('1');
-//         }
-//     })
-// }
+
+    const fileInputs = document.querySelectorAll('[data-upload-input]')
+
+
+    fileInputs.forEach(item => {
+        item.addEventListener('change', (e) => {
+            uploadingFile(item)
+        })
+    });
+
+
+    function uploadingFile(item) {
+        const files = item.files
+
+        const formPreview = item.closest('[data-upload]').querySelector('[data-upload-content]')
+
+        for (let index = 0; index < files.length; index++) {
+                const file = files[index];
+                const reader = new FileReader();
+
+                reader.onload = function (e) {
+                    formPreview.innerHTML +=
+                    `
+                    <div class="form-preview__item">
+                        <img src="${e.target.result}" alt="">
+                        <span>${file.name}</span>
+                    </div>
+                    `
+                }
+
+                reader.readAsDataURL(file)
+            }
+    }
+
+
+
+}
